3050c3982ebfbb8becd3c167090b1fde1e014116
tklawl <tny.ka@hotmail.com>
2020-07-13 17:54:36 +1000

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/* eslint-disable prettier/prettier */
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+  if (context.event.isText === "Hello") {
+    await context.sendText("Hey, there!");
+  } else if (context.event.isText === "GET_STARTED") {
+    await context.sendText("Roll it!");
+  } else if (context.event.isText === "Roll") {
+    await context.sendText("Rolling!");
+  }
 };
